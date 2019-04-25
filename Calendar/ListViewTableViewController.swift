9e403d5c108a2de0dbea59469772bfea5fdad841
Kim, Brian M <198495@s020rmc148-03.local>
2019-04-25 14:19:11 -0500

    Mistakes

--- Calendar/ListViewTableViewController.swift
+++ Calendar/ListViewTableViewController.swift
@@ -5,13 +5,12 @@
 //  Created by Johnson, Joshua A on 4/23/19.
 //  Copyright © 2019 3BMAD. All rights reserved.
 //
-
 import UIKit
 
 class ListViewTableViewController: UITableViewController {
     
     var data: [Event] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,18 +18,15 @@ class ListViewTableViewController: UITableViewController {
         let event1 = Event(name: "Bob", description: "Bob simply is...", date: time1)
         let time2 = Time(day: 05, month: 02, year: 1984, time: 0800)
         let event2 = Event(name: "1984", description: "George Orwell", date: time2)
-
+        
         data = [event1, event2]
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0 {
             return data.count
@@ -42,25 +38,12 @@ class ListViewTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CalendarID", for: indexPath)
         let content = data[indexPath.row]
-        cell.textLabel?.text = content
-        cell.showsReorderControl = true
+        cell.textLabel?.text = content.name
+        cell.detailTextLabel?.text = content.description
         return cell
     }
     
-        cell.textLabel?.text = content.name
-        cell.detailTextLabel?.text = content.description
-        cell.textLabel?.text = content
-        cell.showsReorderControl = true
-            print("Deleted")
-            
-            self.data.remove(at: indexPath.row)
-   
-            self.tableView.deleteRows(at: [indexPath], with: .automatic)
-        }
-    }
     
-    //The random list of strings that the table is displaying as a test
-    let data = ["Hello", "Goodbye", "Watch"]
     
-
+    
 }
