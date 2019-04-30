954769f60fc75d29fc78fb21160a820de7ae37af
Johnson, Joshua A <163660@s020rmc148-04.local>
2019-04-30 14:30:06 -0500

    Josh finished the ordering of the list

--- Calendar/ListViewTableViewController.swift
+++ Calendar/ListViewTableViewController.swift
@@ -15,12 +15,18 @@ class ListViewTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let time1 = Time(day: 21, month: 05, year: 2001, time: 2300)
-        let event1 = Event(name: "Bob", description: "Bob simply is...", date: time1)
-        let time2 = Time(day: 05, month: 02, year: 1984, time: 0800)
+        let time1  = Time(day: 21, month: 05, year: 2001, time: 2300)
+        let event1 = Event(name: "2001", description: "Bob simply is...", date: time1)
+        let time2  = Time(day: 05, month: 02, year: 1984, time: 0800)
         let event2 = Event(name: "1984", description: "George Orwell", date: time2)
+        let time3  = Time(day: 03, month: 03, year: 2005, time: 2100)
+        let event3 = Event(name: "2005", description: "The one time in history that it was April 3rd 2005", date: time3)
+        let time4  = Time(day: 03, month: 04, year: 2005, time: 2100)
+        let event4 = Event(name: "2005e", description: "The 2005 early bird special", date: time4)
 
-        data = [event1, event2]
+        data = [event3, event1, event2, event4]
+        data.sort()
+        print(data)
         navigationItem.leftBarButtonItem = editButtonItem
     }
 
